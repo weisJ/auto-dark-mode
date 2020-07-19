d06286d467e522dfc837199915cf8de7529baa6c
weisj <weisj@arcor.de>
2020-07-19 23:04:05 +0200

    Configure autostyle for project wide consistent formatting.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,18 @@
+import com.github.autostyle.generic.DefaultCopyrightStyle
+import com.github.autostyle.gradle.BaseFormatExtension
 import com.github.vlsi.gradle.crlf.CrLfSpec
 import com.github.vlsi.gradle.crlf.LineEndings
 import com.github.vlsi.gradle.properties.dsl.props
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("com.github.autostyle")
     id("com.github.vlsi.crlf")
     id("com.github.vlsi.gradle-extensions")
     kotlin("jvm") apply false
 }
 
+val skipAutostyle by props()
 val skipJavadoc by props()
 val enableMavenLocal by props()
 val enableGradleMetadata by props()
@@ -16,6 +20,28 @@ val enableGradleMetadata by props()
 val String.v: String get() = rootProject.extra["$this.version"] as String
 
 val buildVersion = "auto-dark-mode".v
+
+fun BaseFormatExtension.license() {
+    licenseHeader(File("${project.rootDir}/LICENSE").readText()) {
+        copyrightStyle("bat", DefaultCopyrightStyle.REM)
+        copyrightStyle("cmd", DefaultCopyrightStyle.REM)
+    }
+    trimTrailingWhitespace()
+    endWithNewline()
+}
+
+fun BaseFormatExtension.configFilter(init: PatternFilterable.() -> Unit) {
+    filter {
+        // Autostyle does not support gitignore yet https://github.com/autostyle/autostyle/issues/13
+        exclude("out/**")
+        if (project == rootProject) {
+            exclude("gradlew*", "gradle/**")
+        } else {
+            exclude("bin/**")
+        }
+        init()
+    }
+}
 
 allprojects {
     group = "com.github.weisj"
@@ -40,6 +66,55 @@ allprojects {
                 accessToken = githubAccessToken
                 manualDownloadUrl =
                     "https://github.com/weisJ/auto-dark-mode/actions?query=workflow%3A%22Build+Native+Libraries%22+is%3Asuccess"
+            }
+        }
+    }
+
+    if (!skipAutostyle) {
+        apply(plugin = "com.github.autostyle")
+        autostyle {
+            kotlinGradle {
+                ktlint()
+            }
+            format("properties") {
+                configFilter {
+                    include("**/*.properties")
+                    exclude("**/gradle.properties")
+                }
+                license()
+            }
+            format("configs") {
+                configFilter {
+                    include("**/*.sh", "**/*.bsh", "**/*.cmd", "**/*.bat")
+                    include("**/*.xsd", "**/*.xsl", "**/*.xml")
+                    exclude("*.eclipseformat.xml")
+                }
+                license()
+            }
+            format("markdown") {
+                filter.include("**/*.md")
+                endWithNewline()
+            }
+            cpp {
+                trimTrailingWhitespace()
+                endWithNewline()
+                license()
+                eclipse {
+                    configFile("${project.rootDir}/cpp.eclipseformat.xml")
+                }
+            }
+        }
+
+        plugins.withType<JavaPlugin> {
+            autostyle {
+                java {
+                    importOrder("java", "javax", "org", "com")
+                    removeUnusedImports()
+                    license()
+                    eclipse {
+                        configFile("${project.rootDir}/java.eclipseformat.xml")
+                    }
+                }
             }
         }
     }
