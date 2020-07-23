4bc18dd0150d9720c28d9c3d4e101beff6e0783c
IncPlusPlus <cloherty.ryan@gmail.com>
2020-07-23 17:55:23 -0400

    Remove unnecessary directives from the Gnome build.gradle.kts

--- linux/gnome/build.gradle.kts
+++ linux/gnome/build.gradle.kts
@@ -1,5 +1,4 @@
 import JniUtils.asVariantName
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
@@ -68,17 +67,7 @@ library {
         }
     }
 }
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-}
-repositories {
-    mavenCentral()
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
