30568b639c6f95e8fb45c079099766c38733bf50
IncPlusPlus <cloherty.ryan@gmail.com>
2020-07-28 22:23:30 -0400

    Added GnomeThemeUtils
    This adds a method that returns the installed GTK themes. It also adds another build-time dependency (libgtk-3-dev).

--- linux/gnome/build.gradle.kts
+++ linux/gnome/build.gradle.kts
@@ -21,20 +21,38 @@ library {
             compileTasks.configureEach {
                 compilerArgs.addAll(toolChain.map {
                     when (it) {
-                        // These parameters (excluding --std=c++11) were obtained with `pkg-config --cflags glibmm-2.4 giomm-2.4 sigc++-2.0`.
+                        // These parameters (excluding --std=c++11) were obtained with `pkg-config --cflags glibmm-2.4 giomm-2.4 sigc++-2.0 gtk+-3.0`.
                         is Gcc, is Clang -> listOf(
                             "--std=c++11",
                             "-pthread",
                             "-I/usr/include/giomm-2.4",
                             "-I/usr/lib/x86_64-linux-gnu/giomm-2.4/include",
-                            "-I/usr/include/libmount",
-                            "-I/usr/include/blkid",
                             "-I/usr/include/glibmm-2.4",
                             "-I/usr/lib/x86_64-linux-gnu/glibmm-2.4/include",
-                            "-I/usr/include/glib-2.0",
-                            "-I/usr/lib/x86_64-linux-gnu/glib-2.0/include",
                             "-I/usr/include/sigc++-2.0",
-                            "-I/usr/lib/x86_64-linux-gnu/sigc++-2.0/include"
+                            "-I/usr/lib/x86_64-linux-gnu/sigc++-2.0/include",
+                            "-I/usr/include/gtk-3.0",
+                            "-I/usr/include/at-spi2-atk/2.0",
+                            "-I/usr/include/at-spi-2.0",
+                            "-I/usr/include/dbus-1.0",
+                            "-I/usr/lib/x86_64-linux-gnu/dbus-1.0/include",
+                            "-I/usr/include/gtk-3.0",
+                            "-I/usr/include/gio-unix-2.0",
+                            "-I/usr/include/cairo",
+                            "-I/usr/include/pango-1.0",
+                            "-I/usr/include/fribidi",
+                            "-I/usr/include/harfbuzz",
+                            "-I/usr/include/atk-1.0",
+                            "-I/usr/include/cairo",
+                            "-I/usr/include/pixman-1",
+                            "-I/usr/include/uuid",
+                            "-I/usr/include/freetype2",
+                            "-I/usr/include/libpng16",
+                            "-I/usr/include/gdk-pixbuf-2.0",
+                            "-I/usr/include/libmount",
+                            "-I/usr/include/blkid",
+                            "-I/usr/include/glib-2.0",
+                            "-I/usr/lib/x86_64-linux-gnu/glib-2.0/include"
                         )
                         else -> emptyList()
                     }
@@ -51,14 +69,23 @@ library {
             linkTask.configure {
                 linkerArgs.addAll(toolChain.map {
                     when (it) {
-                        // These parameters were obtained with `pkg-config --libs glibmm-2.4 giomm-2.4 sigc++-2.0`.
+                        // These parameters were obtained with `pkg-config --libs glibmm-2.4 giomm-2.4 sigc++-2.0 gtk+-3.0`.
                         is Gcc, is Clang -> listOf(
                             "-lgiomm-2.4",
-                            "-lgio-2.0",
                             "-lglibmm-2.4",
+                            "-lsigc-2.0",
+                            "-lgtk-3",
+                            "-lgdk-3",
+                            "-lpangocairo-1.0",
+                            "-lpango-1.0",
+                            "-lharfbuzz",
+                            "-latk-1.0",
+                            "-lcairo-gobject",
+                            "-lcairo",
+                            "-lgdk_pixbuf-2.0",
+                            "-lgio-2.0",
                             "-lgobject-2.0",
-                            "-lglib-2.0",
-                            "-lsigc-2.0"
+                            "-lglib-2.0"
                         )
                         else -> emptyList()
                     }
