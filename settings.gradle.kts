31fd8d931c9b89c6b4265743d13083b7533f4acc
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2022-03-29 21:12:49 +0200

    Replace autostyle with spotless

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         fun String.v() = extra["$this.version"].toString()
         fun PluginDependenciesSpec.idv(id: String, key: String = id) = id(id) version key.v()
 
-        idv("com.github.autostyle")
+        idv("com.diffplug.spotless")
         idv("com.github.vlsi.crlf", "com.github.vlsi.vlsi-release-plugins")
         idv("com.github.vlsi.gradle-extensions", "com.github.vlsi.vlsi-release-plugins")
         idv("com.github.vlsi.ide", "com.github.vlsi.vlsi-release-plugins")
